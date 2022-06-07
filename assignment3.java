d7a3bebfc67781d2c5eba1e435684684e560b5ff
aditya bansal <43151984+adityabansal567@users.noreply.github.com>
2022-06-07 15:36:41 +0530

    Update assignment3.java

--- assignment3.java
+++ assignment3.java
@@ -5,29 +5,31 @@ public class assignment3 {
 	    	System.out.println("enter number");
 	        Scanner sc=new Scanner(System.in);
 	        int object=sc.nextInt();
-	        // for string ----
-	      //  String a = sc.nextLine();
+	        // for Double ----
+	       double a = sc.nextDouble();
 	       convertToWrapper(object);  //for integer
-	      // convertToWrapper(a);
+	      convertToWrapper(a);
 	       
 	    }
-//	    private static String convertToWrapper(String a) {
-//	    	String ab = String.valueOf(a);
-//	        if(a instanceof  String){
-//	            System.out.println("Converted value is "+ ab);
-//	        }
-//			return ab;
+    private static  double convertToWrapper(Double a) {
+	    	double ab = Double.valueOf(a);
+	        if(a instanceof  Double){
+	            System.out.println("Converted value is "+ ab);
+	        }
+			return ab;
 			                                          //like this we can do that for other primitive data types for float, long ...
-//		}
+		}
 		public static  Integer convertToWrapper(int object){
 	        Integer abc = Integer.valueOf(object);
 	        System.out.println(abc);
+	        
 	        if(abc instanceof Integer){
 	            System.out.println("Converted value is "+ abc);
 	        }
 	        else {
 	        	System.out.println("give proper input");
 	        }
+	        
 			return abc;
 	    }
 	}
